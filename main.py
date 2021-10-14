aea04095035ac1d47f088ef78d51b84e9cf45e77
Santiago Linares <santilinares@hotmail.es>
2021-10-14 15:38:47 -0300

    Primer acercamiento a crear la base de datos

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 from fastapi import FastAPI
 import pony.orm as pony
 
+db = pony.Database()
+
+class Partida(db.Entity):
+    id_partida = pony.PrimaryKey(int, auto=True)
+    nombre = pony.Required(str)
+    iniciada = pony.Required(bool) 
+    creador = pony.Required("Jugadores", reverse='creador_de')
+    jugadores = pony.Set("Jugadores", reverse='partida')
+
+class Jugadores(db.Entity):
+    id_jugador = pony.PrimaryKey(int, auto=True)
+    apodo = pony.Required(str)
+    creador_de = pony.Set("Partida", reverse='creador')
+    partida = pony.Set("Partida", reverse='jugadores')
+
+
+db.bind('sqlite', 'database.sqlite', create_db=True)
+db.generate_mapping(create_tables=True)
+
 app = FastAPI()
 
 @app.get("/home")
