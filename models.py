8bfed5837abeb4e39d872d545f6be4535a82ffbb
PelePeluche <fernandopffarias@gmail.com>
2021-10-19 23:20:09 -0300

    Agregadas las funciones "crear_jugador", "asociar_a_partida" y "crear_partida"

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ import pony.orm as pony
 
 db = pony.Database()
 
+#implementación de clases
 class Partida(db.Entity):
     id_partida = pony.PrimaryKey(int, auto=True)
     nombre = pony.Required(str)
@@ -14,9 +15,30 @@ class Jugador(db.Entity):
     id_jugador = pony.PrimaryKey(int, auto=True)
     apodo = pony.Required(str)
     orden_turno = pony.Optional(int)
-    creador_de = pony.Set("Partida", reverse='creador')
-    partida = pony.Set("Partida", reverse='jugadores')
+    creador_de = pony.Optional("Partida", reverse='creador')
+    partida = pony.Optional("Partida", reverse='jugadores')
 
 
+# línea que sirve para debug
+pony.set_sql_debug(True)
+
+#creación de tablas para los modelos
 db.bind('sqlite', 'database.sqlite', create_db=True)
 db.generate_mapping(create_tables=True)
+
+#implementación de funciones
+@pony.db_session()
+def crear_jugador(apodo):
+    jugador = Jugador(apodo=apodo)
+    return jugador
+
+@pony.db_session()
+def asociar_a_partida(partida, jugador):
+    partida.jugadores.add(jugador)
+
+@pony.db_session()
+def crear_partida(nombre, jugador):
+    partida = Partida(nombre=nombre, creador=jugador.id_jugador)
+    asociar_a_partida(partida, jugador)
+    return partida
+
