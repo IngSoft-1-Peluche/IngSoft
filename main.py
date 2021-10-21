16da856371b54242aa4342a6323d450bde332be8
Santiago Linares <santilinares@hotmail.es>
2021-10-21 15:01:16 -0300

    Implementa endpoint crear_partida y actualiza tests

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI, status
 import pony.orm as pony
+from pydantic import BaseModel
 
-from models import db
+from models import db , crear_jugador, crear_partida
 
 app = FastAPI()
+
+class PartidaIn(BaseModel):
+    nombre_partida: str
+    apodo: str
+
+class PartidaOut(BaseModel):
+    id_partida: int
+    nombre_partida: str
+    id_jugador: int
+    apodo: str
+    jugador_creador: bool
 
 @app.get("/home")
 async def home():
@@ -20,3 +32,19 @@ async def listar_partidas():
             'nombre_partida': p.nombre,
             'cantidad_jugadores': len(p.jugadores)
                 } for p in partidas]
+
+@app.post("/partidas/", 
+    response_model=PartidaOut,
+    status_code=status.HTTP_201_CREATED
+)
+async def respuesta_creacion(nueva_partida: PartidaIn) -> int:
+    nueva_partida_dicionario = nueva_partida.dict()
+    jugador = crear_jugador(nueva_partida_dicionario["apodo"])
+    partida = crear_partida(nueva_partida_dicionario["nombre_partida"],jugador)
+
+    return PartidaOut(
+        id_partida= partida.id_partida,
+        nombre_partida = partida.nombre,
+        id_jugador = jugador.id_jugador,
+        apodo = jugador.apodo,
+        jugador_creador = True)
--- test_main.py
+++ test_main.py
@@ -15,21 +15,22 @@ def test_get_home_page():
 @pony.db_session
 def test_database():
     j1 = db.Jugador(apodo='juan')
+    j2 = db.Jugador(apodo='maria')
+    pony.flush()
     p1 = db.Partida(nombre='Partida de juan', iniciada=False, creador=j1)
     j1.partida = p1 
-    j2 = db.Jugador(apodo='maria')
     j2.partida = p1
 
     pony.commit()#Es necesario realizar los commit para que se guarden las entidades en la base y se les asigne un id
     assert p1.nombre == 'Partida de juan'
     assert p1.iniciada == False
-    assert p1 in j1.creador_de
+    assert p1 == j1.creador_de
     assert j1 in p1.jugadores
     assert p1.jugador_en_turno == None
     assert j1.orden_turno == None
     assert j1 in p1.jugadores and j2 in p1.jugadores
     assert j1.id_jugador != j2.id_jugador
-    assert j2.creador_de == []
+    assert j2.creador_de == None
 
 @pony.db_session
 def test_listar_partidas_endpoint():
@@ -40,3 +41,11 @@ def test_listar_partidas_endpoint():
     assert response.status_code == status.HTTP_200_OK
     assert all(p['cantidad_jugadores'] < 6 for p in partidas_json)
     assert all(not p.iniciada for p in partidas)
+
+def test_post_crear_partida():
+    response = client.post("/partidas/")
+    response = client.post(
+        "/partidas/",
+        json={"nombre_partida": "foobar", "apodo": "Foo Bar"},
+    )
+    assert response.status_code == status.HTTP_201_CREATED
