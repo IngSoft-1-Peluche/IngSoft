71d259c1b7334f6b6b4f118758fb2729ad94fa2a
PelePeluche <fernandopffarias@gmail.com>
2021-10-23 11:08:57 -0300

    Cambios para permisos de CORS

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI, status
 import pony.orm as pony
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 from models import db , crear_jugador, crear_partida
 
 app = FastAPI()
+
+#Permisos para fetch de Front
+app.add_middleware(CORSMiddleware,
+allow_origins=["*"],
+allow_credentials=True,
+allow_methods=["*"],
+allow_headers=["*"],)
 
 class PartidaIn(BaseModel):
     nombre_partida: str
