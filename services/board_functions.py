e38d1d9f1df974d9298956d3cceb7841b96d7964
Gonzalo Zigaran <gjzigaran@gmail.com>
2021-11-02 20:22:37 -0300

    funcion que da las casillas posibles a moverse a partir del numero del dado

---
+++ services/board_functions.py
@@ -0,0 +1,10 @@
+import pony.orm as pony
+
+from board.board import *
+
+def posiciones_posibles_a_mover(posicion_inicial, numero_dado):
+    posiciones_posibles = []
+    matriz_movimientos = MOVIMIENTOS[numero_dado]
+    for i in np.where(matriz_movimientos[:,posicion_inicial] > 0)[0]:
+        posiciones_posibles.append(i)
+    return posiciones_posibles
