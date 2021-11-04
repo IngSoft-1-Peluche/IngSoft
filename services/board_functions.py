91f1f409ce00edf9412dbb85681c86bc8bb4e222
Gonzalo Zigaran <gjzigaran@gmail.com>
2021-11-04 16:51:17 -0300

    estilo de codigo

--- services/board_functions.py
+++ services/board_functions.py
@@ -2,9 +2,10 @@ import pony.orm as pony
 
 from board.board import *
 
+
 def posiciones_posibles_a_mover(posicion_inicial, numero_dado):
     posiciones_posibles = []
     matriz_movimientos = MOVIMIENTOS[numero_dado]
-    for i in np.where(matriz_movimientos[:,posicion_inicial] > 0)[0]:
+    for i in np.where(matriz_movimientos[:, posicion_inicial] > 0)[0]:
         posiciones_posibles.append(i)
     return sorted(posiciones_posibles)
