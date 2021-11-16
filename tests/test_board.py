955bbcd1c2f0490e0663da42796f4fff76fc3222
Gonzalo Zigaran <gjzigaran@gmail.com>
2021-11-16 09:52:14 -0300

    tests de movimiento con casillas especiales

--- tests/test_board.py
+++ tests/test_board.py
@@ -5,3 +5,8 @@ def test_posiciones_posibles_a_mover():
     assert posiciones_posibles_a_mover(83, 1) == [81, 83]
     assert posiciones_posibles_a_mover(69, 2) == [67, 68, 69]
     assert posiciones_posibles_a_mover(58, 3) == [48, 55, 56, 57, 58, 59, 60, 61, 71, 72]
+
+
+def test_posiciones_posibles_a_mover_casilla_especial():
+    assert posiciones_posibles_a_mover(10, 2) == [1, 6, 8, 10, 12, 14, 71]
+    assert posiciones_posibles_a_mover(54, 1) == [53, 54, 55, 65]
